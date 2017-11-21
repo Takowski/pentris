a8bf3538f6b2d9b2235e16fb78929e06ed6bded4
WizardCream <32612120+WizardCream@users.noreply.github.com>
2017-11-21 14:29:17 +0100

    Add files via upload

---
+++ DropDown.java
@@ -0,0 +1,11 @@
+public class DropDown{
+	public void DropDown(){
+		if(!collide(x, y+1, parameter for if the shape is rotated or mirrored?){
+			y+1;
+		}
+		else{
+			place where is;
+		}
+		update board;
+	}
+}
